e73f64eed3cd620023d1805edda8cdd253e90e04
Stankim <stankim.ks@gmail.com>
2023-08-25 12:38:28 +0300

    added camera

--- lighting.cpp
+++ lighting.cpp
@@ -111,8 +111,6 @@ glBindVertexArray(0);
 
 
 
-glm::mat4 view = glm::mat4(1.0f);
-view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
 
 glm::mat4 projection;
 projection = glm::perspective(glm::radians(45.0f), 800.0f/600.0f, 0.0f, 100.0f);  
@@ -125,15 +123,20 @@ projection = glm::perspective(glm::radians(45.0f), 800.0f/600.0f, 0.0f, 100.0f);
 
 
 
-
+const float radius = 10.0f;
 while(!glfwWindowShouldClose(window)){
 processInput(window);
 glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
 glClear(GL_COLOR_BUFFER_BIT);
 
+float camX = sin(glfwGetTime()) * radius;
+float camZ = cos(glfwGetTime()) * radius;
+glm::mat4 view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.5, 1.0, 0.0));
+
+
 
 glm::mat4 model = glm::mat4(1.0f);
-model = glm::rotate(model, (float)glfwGetTime() * glm::radians(20.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+model = glm::rotate(model, (float)glfwGetTime() * glm::radians(20.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 ownShader.use();
 ownShader.setMat4("model", model);
 ownShader.setMat4("view", view);
