706342abc1996d0cb862f4fc6d5bc92a903f9a19
Stankim <stankim.ks@gmail.com>
2023-09-01 16:41:38 +0300

    added scroll view to sphere

--- sphere/drawSphere.cpp
+++ sphere/drawSphere.cpp
@@ -2,24 +2,25 @@
 #include<GLFW/glfw3.h>
 #include<iostream>
 #include "Sphere.cpp"
+#include "./../shaders.cpp"
 
 void framebuffer_size_callback(GLFWwindow *window, int width,int height);
 void processInput(GLFWwindow *window);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+float fov=45.0f;
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 700;
 
-const char *vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"void main()\n"
-"{\n"
-"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-"}\n\0";
-const char *fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-"}\n\0";
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+float yaw=-90.0f, pitch;
+bool firstMouse = true;
+float lastX = SCR_WIDTH / 2.0f , lastY= SCR_HEIGHT / 2.0f;
 
 int main()
 {
@@ -37,6 +38,11 @@ return -1;
 }
 glfwMakeContextCurrent(window);
 glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+glfwSetCursorPosCallback(window, mouse_callback);
+
+glfwSetScrollCallback(window, scroll_callback);
+
 
 
 if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -44,46 +50,12 @@ if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 std::cout << "Failed to initialize GLAD" << std::endl;
 return -1;
 }
+//glEnable(GL_DEPTH_TEST);
 
-unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-glCompileShader(vertexShader);
+Shader sphereShader = Shader("sphere.vs", "sphere.fs");
 
-int success;
-char infoLog[512];
-glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-if(!success){
-glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-std::cout << "ERROR: vertex shader compilation failed\n"<< infoLog<< std::endl;
-}
+Sphere sphere(9);
 
-unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-glCompileShader(fragmentShader);
-
-glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-if(!success)
-{
-glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-std::cout<< "ERROR: Fragment shader compilation failed\n" << infoLog << std::endl;
-}
-
-unsigned int shaderProgram = glCreateProgram();
-glAttachShader(shaderProgram, vertexShader);
-glAttachShader(shaderProgram, fragmentShader);
-glLinkProgram(shaderProgram);
-
-glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-if(!success) {
-glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-std::cout << "ERROR:: Shader program linkage failed\n" << infoLog << std::endl;
-}
-glDeleteShader(vertexShader);
-glDeleteShader(fragmentShader);
-
-Sphere sphere(2);
-
-sphere.vertices;
 
 unsigned int VBO, VAO;
 glGenVertexArrays(1, &VAO);
@@ -99,10 +71,29 @@ glBindVertexArray(0);
 glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 while(!glfwWindowShouldClose(window)){
+float currentFrame = static_cast<float>(glfwGetTime());
+deltaTime = currentFrame - lastFrame;
+lastFrame = currentFrame;
+
+
 processInput(window);
 glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
 glClear(GL_COLOR_BUFFER_BIT);
-glUseProgram(shaderProgram);
+
+glm::mat4 projection;
+projection = glm::perspective(glm::radians(fov), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.01f, 100.0f); 
+
+glm::mat4 view = glm::lookAt(cameraPos, cameraPos+cameraFront, cameraUp);
+
+glm::mat4 model = glm::mat4(1.0f);
+
+
+sphereShader.use();
+
+sphereShader.setMat4("model", model);
+sphereShader.setMat4("view", view);	
+sphereShader.setMat4("projection", projection);
+
 glBindVertexArray(VAO);
 glDrawArrays(GL_TRIANGLES, 0, sphere.number_of_vertices);
 glfwSwapBuffers(window);
@@ -111,17 +102,70 @@ glfwPollEvents();
 
 glDeleteVertexArrays(1, &VAO);
 glDeleteBuffers(1, &VBO);
-glDeleteProgram(shaderProgram);
+glDeleteProgram(sphereShader.ID);
 
 glfwTerminate();
 return 0;
 }
 
+
 void processInput(GLFWwindow *window){
 if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 glfwSetWindowShouldClose(window, true);
+const float cameraSpeed = 2.5f * deltaTime;
+if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+	cameraPos += cameraSpeed * cameraFront;
+if(glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+	cameraPos -= cameraSpeed * cameraFront;
+if(glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+	cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+if(glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+	cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 }
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height){
 glViewport(0,0, width, height);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+  
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; 
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw   += xoffset;
+    pitch += yoffset;
+
+    if(pitch > 89.0f)
+        pitch = 89.0f;
+    if(pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 direction;
+    direction.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    direction.y = sin(glm::radians(pitch));
+    direction.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    
+    cameraFront = glm::normalize(direction);
+    
+} 
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset){
+fov -= (float) yoffset;
+if(fov<1.0f)
+	fov = 1.0f;
+if(fov > 45.0f)
+	fov = 45.0f;
 }
